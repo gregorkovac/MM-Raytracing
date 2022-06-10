434b995bec1167c5c1778c5695fd9cd46780a0c1
jovanav25 <jv8043@student.uni-lj.si>
2022-06-10 14:39:31 +0200

    raytracing with camera 1.0

--- test_raytracing.m
+++ test_raytracing.m
@@ -1,25 +1,25 @@
 % initializing sphere with the center in (0, 0, -1) and with radius 0.5
-f = @(x, y, z) (x - 0).^2 + (y - 0).^2 + (z + 1).^2 - 0.5.^2;
+f = @(x, y, z) (x - 0).^2 + (y + 0.5).^2 + (z + 1).^2 - 0.3.^2;
 % computing partial derivatives to the parametrization of the sphere
 dfdx = @(x, y, z) 2.*x;
-dfdy = @(x, y, z) 2.*y;
+dfdy = @(x, y, z) 2.*(y + 0.5);
 dfdz = @(x, y, z) 2.*(z + 1);
 
 % initializing sphere with the center in (0, 0, -1) and with radius 0.5
-f1 = @(x, y, z) (x - 0).^2 + (y - 0.5).^2 + (z + 1).^2 - 0.5.^2;
+f1 = @(x, y, z) (x - 0).^2 + (y - 0.5).^2 + (z + 1).^2 - 0.3.^2;
 % computing partial derivatives to the parametrization of the sphere
 df1dx = @(x, y, z) 2.*x;
 df1dy = @(x, y, z) 2.*(y - 0.5);
 df1dz = @(x, y, z) 2.*(z + 1);
 
 
-image_width = 80;
-image_height = 60;
+image_width = 400;
+image_height = 300;
 %initializing white image
 image = ones(image_height, image_width, 3); 
 
 %initializing origin of rayes
-lightOrigin = [0; 0.5; 0];
+lightOrigin = [0; 0; 0];
 origin = [0; 0; 0];
 origin_height = 2;
 origin_width = origin_height.*image_width/image_height; 
@@ -36,8 +36,16 @@ for i=1:image_height
     v = i / image_height;
 
     direction_vector = lower_left_corner + u.*horizontal + v.*vertical - origin;
- 
-    image(i, j, :) = raytracing(f, f1, dfdx, dfdy, dfdz, df1dx, df1dy, df1dz, origin, direction_vector, lightOrigin, 0.01, 100, 10);
+    
+    def_colors = [1 0 0; 1 0 0]';
+    image(i, j, :) = raytracing(def_colors, f, f1, dfdx, dfdy, dfdz, df1dx, df1dy, df1dz, origin, direction_vector, lightOrigin, 0.01, 100, 10);
     end
 end
+
+for i=2:image_height-1
+    for j=2:image_width-1
+        image(i, j, :) = 0.5.* image(i,j, :) + (image(i + 1, j, :) + image(i + 1, j + 1, :) + image(i + 1,j - 1, :) + image(i - 1, j, :) + image(i - 1, j + 1, :) + image(i - 1, j - 1, :) + image(i, j - 1, :) + image(i, j + 1))./16;
+    end
+end
+
 imshow(image)
