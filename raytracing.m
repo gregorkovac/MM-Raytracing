1374355e009c8cf17d329cc254568c09746eabe5
jovanav25 <jv8043@student.uni-lj.si>
2022-06-11 11:44:29 +0200

    cleaning up code

--- raytracing.m
+++ raytracing.m
@@ -5,6 +5,8 @@ function color = raytracing(def_colors, f, f1, dfdx, dfdy, dfdz, df1dx, df1dy, d
 % step is the amount the ray moves in every iteration
 % maxIter is the maximum number of iterations
 % maxRef is the maximum number of allowed reflections
+% function returns a 3x1 vector color, representing the color of the
+% point intersection of ray and object
 
 % initializing color to white
 color = [0; 0; 0];
@@ -16,23 +18,12 @@ def_color = def_colors(:, 1);
 fz = @(x, y) -f(x, y, 0)./f(0, 0, 1);
 f1z = @(x, y) -f1(x, y, 0)./f1(0, 0, 1);
 
+% computing functions and derivatives needed for Newton's method
 g = @(t) f(T0(1) + v(1)*t, T0(2) + v(2)*t, T0(3) + v(3)*t);
 gdot = @(t) v(1)*dfdx(T0(1) + v(1)*t, T0(2) + v(2)*t, T0(3) + v(3)*t) + v(2)*dfdy(T0(1) + v(1)*t, T0(2) + v(2)*t, T0(3) + v(3)*t) + v(3)*dfdz(T0(1) + v(1)*t, T0(2) + v(2)*t, T0(3) + v(3)*t);
 
 g1 = @(t) f1(T0(1) + v(1)*t, T0(2) + v(2)*t, T0(3) + v(3)*t);
 g1dot = @(t) v(1)*df1dx(T0(1) + v(1)*t, T0(2) + v(2)*t, T0(3) + v(3)*t) + v(2)*df1dy(T0(1) + v(1)*t, T0(2) + v(2)*t, T0(3) + v(3)*t) + v(3)*df1dz(T0(1) + v(1)*t, T0(2) + v(2)*t, T0(3) + v(3)*t);
-
-
-% plot the plane
-%{
-axis equal;
-x = [10 -10 -10 10];
-y = [10 10 -10 -10];
-[x y] = meshgrid(x, y);
-z = fz(x, y);
-surf(x, y, z);
-hold on
-%}
 
 % initialize the vector of hit points as empty
 T = [];
@@ -61,9 +52,6 @@ ret = [];
 % while the sign remains unchanged and the number of iterations is smaller
 % then the maximum number of iterations...
 while (iter <= maxIter)
-   % plot the current point
-   %plot3(currT(1), currT(2), currT(3), '.r', 'markersize', 10);
-  
    % increment the number of iterations
    iter = iter + 1;
   
@@ -84,57 +72,52 @@ while (iter <= maxIter)
    currSign1 = sign(f1(currT(1), currT(2), currT(3)));
    
    if (currSign ~= prevSign)
-     % define a starting approximation
-  startingApproximation = (t + (t - step)) / 2;
+    % define a starting approximation
+    startingApproximation = (t + (t - step)) / 2;
   
-  % use Newton's method to get a better approximation of the parameter
-  u = newton(g, gdot, startingApproximation, 1e-10, 100);
+    % use Newton's method to get a better approximation of the parameter
+    u = newton(g, gdot, startingApproximation, 1e-10, 100);
   
-  % determine the better hit point
-  U = T0 + v*u;
+    % determine the better hit point
+    U = T0 + v*u;
   
-  % add the hit point to the list of hit points
-  T = [T; U];
+    % add the hit point to the list of hit points
+    T = [T; U];
   
-  % plot the hit point
-  % plot3(U(1), U(2), U(3), '.m', 'markersize', 30);
-  cos_reflAngle = reflectionAngle(v, U, lightOrigin, dfdx, dfdy, dfdz);
-  def_color = def_colors(:, 1);
+    % compute the angle between the normal and lightsource
+    cos_reflAngle = reflectionAngle(v, U, lightOrigin, dfdx, dfdy, dfdz);
+    
+    % choose color for coloring the point
+    def_color = def_colors(:, 1);
   break;
   end
  
-    if (currSign1 ~= prevSign1)
-     % define a starting approximation
-  startingApproximation = (t + (t - step)) / 2;
+  if (currSign1 ~= prevSign1)
+    % define a starting approximation
+    startingApproximation = (t + (t - step)) / 2;
   
-  % use Newton's method to get a better approximation of the parameter
-  u = newton(g1, g1dot, startingApproximation, 1e-10, 100);
+    % use Newton's method to get a better approximation of the parameter
+    u = newton(g1, g1dot, startingApproximation, 1e-10, 100);
   
-  % determine the better hit point
-  U = T0 + v*u;
+    % determine the better hit point
+    U = T0 + v*u;
   
-  % add the hit point to the list of hit points
-  T = [T; U];
+    % add the hit point to the list of hit points
+    T = [T; U];
 
- 
-
-  if (testRef == 0)
-  ret = raytracing(def_color, f, @(x,y,z) 0, dfdx, dfdy, dfdz, @(x,y,z) 0, @(x,y,z) 0, @(x,y,z) 0, U, [lightOrigin(1) - U(1); lightOrigin(2) - U(2); lightOrigin(3) - U(3)], lightOrigin, step, maxIter, maxRef, 1);
-  end
-
-
-  
-  % plot the hit point
-  % plot3(U(1), U(2), U(3), '.m', 'markersize', 30);
-
-  cos_reflAngle = reflectionAngle(v, U, lightOrigin, df1dx, df1dy, df1dz);
-  def_color = def_colors(:, 2);
-  break;
+    if (testRef == 0)
+        ret = raytracing(def_colors, f, @(x,y,z) 0, dfdx, dfdy, dfdz, @(x,y,z) 0, @(x,y,z) 0, @(x,y,z) 0, U, lightOrigin - U, lightOrigin, step, maxIter, maxRef, 1);
     end
    
-end;
+    % computing the angle between reflection vector and the light source
+    cos_reflAngle = reflectionAngle(v, U, lightOrigin, df1dx, df1dy, df1dz);
+    def_color = def_colors(:, 2);
+    break;
+    end
+   
+end
 
-% if a hit point is found, use Newton's iteration to get a better approximation
+% if there are more iterations than allowed, set cos of angle to 0
 if (iter > maxIter)
   cos_reflAngle = 0;
 end
@@ -170,40 +153,31 @@ end
 end
 
 function cosf = reflectionAngle(v, T, lightOrigin, dfdx, dfdy, dfdz)  
-  %fi = reflectionAngle(f, T) calculates the angle ... todo
-  
+  % fi = reflectionAngle(f, T) calculates the angle 
+  % needed for determining the color in raytracing.  
+  % Depeneding on the chosen option it can calculate angle:
+  %    1. between normal vector and light source 
+  %    2. between reflection vector of the ray and light source 
+
   % n is a normal to the plane from point T
-  n = [ dfdx(T(1), T(2), T(3));  feval(dfdy, T(1), T(2), T(3)); feval(dfdz, T(1), T(2), T(3))];
-  %G = [ n(1) + T(1); n(2) + T(2); n(3) + T(3)];
-   %plot3(G(1), G(2), G(3), '*k', 'markersize', 30);
+  n = [feval(dfdx, T(1), T(2), T(3));  feval(dfdy, T(1), T(2), T(3)); feval(dfdz, T(1), T(2), T(3))];
+  %G = n + T
   
-  %hold on;
-  %plot3(n(1), n(2), n(3), '.r', 'markersize', 30);
-  %quiver3(T(1), T(2), T(3), n(1), n(2), n(3),'k')
-  %hold on
+  % normalizing n
+  n = n./norm(n); 
+
+  % computing the reflection vector
+  vn = 2*dot(v, n);
+  p = vn .* n;
+  r = v - p;
   
- 
+  % computing the vector between the light origin and the intersection of
+  % ray from the camera with the object
+  lvec = lightOrigin - T;
+    
+  % first option
+  cosf = dot(n, lvec) ./ (norm(n) .* norm(lvec));
   
-  
-  %normalize n
-  sizen = n(1) *n(1) + n(2) * n(2) + n(3) * n(3) ;
-  n = [n(1) / sqrt(sizen);n(2) / sqrt(sizen); n(3) / sqrt(sizen)];
-  vn = 2*(v(1) * n(1) + v(2) * n(2) + v(3) * n(3));
-  p = [vn * n(1); vn * n(2); vn * n(3)];
-  
-  r = [v(1) - p(1); v(2) - p(2); v(3) - p(3)];
-   
-  lvec = [lightOrigin(1) - T(1); lightOrigin(2) - T(2); lightOrigin(3) - T(3)];
-  
-  
-  %r = [(2*s) / (s.^2 + t.^2 + 1); (2*t) / (s.^2 + t.^2 + 1); 
-   %   (-1 + s.^2 + t.^2 ) / (s.^2 + t.^2 + 1)]
-  %quiver3(T(1), T(2), T(3), r(1), r(2), r(3),'y')
-  %hold on   
-  
-  % 
-  % cosf = (n(1)*r(1) + n(2)*r(2) + n(3)*r(3)) / ((n(1)*n(1) + n(2)*n(2) + n(3)*n(3)) * (r(1)*r(1) + r(2)*r(2) + r(3)*r(3)));
-  % cosf = (r(1)*lvec(1) + r(2)*lvec(2) + r(3)*lvec(3)) / sqrt((r(1) * r(1) + r(2) * r(2) + r(3) * r(3))) * sqrt((lvec(1) * lvec(1) + lvec(2) * lvec(2) + lvec(3) * lvec(3)));
- 
-  cosf = (n(1).*lvec(1) + n(2).*lvec(2) + n(3).*lvec(3)) ./ (norm(n) .* norm(lvec));
+  % second option 
+  % cosf = dot(r, lvec) ./ (norm(r) .* norm(lvec));
 end
